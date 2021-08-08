f4f762049f73e845c8d88e5723ebbd8a4385ff89
Michael Fienen <fienen@gmail.com>
2021-08-07 23:37:26 -0500

    WEB-9422: Build to better directory and copy for Fractal

--- scss/rollup.config.js
+++ scss/rollup.config.js
@@ -1,20 +1,38 @@
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import postcss from 'postcss';
+import postcssImport from 'postcss-import';
 import scss from 'rollup-plugin-scss';
+
+const target = process.env.TARGET;
+
+const postcssConfig = [
+  postcssImport(),
+  autoprefixer({
+    overrideBrowserslist: 'last 4 versions',
+  })
+];
+
+// If we are in production mode, then add cssnano to minify CSS
+if (target === 'production') {
+	postcssConfig.push(
+		cssnano({
+			preset: 'default',
+		})
+	);
+} 
 
 export default {
   input: 'entry.js',
+  output: false,
   plugins: [
     scss({
       includePaths: [
           'node_modules/'
         ],
-      output: '../public/css/dist/styles.css',
-      processor: () => postcss([
-        autoprefixer(),
-        cssnano()
-      ]),
+      output: 'dist/styles.css',
+      processor: () => postcss(postcssConfig),
+      sourceMap: (target === 'development' ? true : false)
     })
   ]
 }
