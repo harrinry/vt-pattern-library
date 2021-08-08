91e004743ba4f29fcdc9d588fe5586e8898c359e
Michael Fienen <fienen@gmail.com>
2021-08-07 21:47:22 -0500

    WEB-9422: Restore base working file

--- scss/rollup.config.js
+++ scss/rollup.config.js
@@ -1,18 +1,20 @@
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import postcss from 'rollup-plugin-postcss';
+import postcss from 'postcss';
 import scss from 'rollup-plugin-scss';
-
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
 export default {
   input: 'entry.js',
   plugins: [
     scss({
-      includePaths: ['node_modules/'],
+      includePaths: [
+          'node_modules/'
+        ],
       output: '../public/css/dist/styles.css',
-      processor: () => postcss(),
-      sourceMap: (process.env.NODE_ENV == 'development' ? true : false)
+      processor: () => postcss([
+        autoprefixer(),
+        cssnano()
+      ]),
     })
   ]
 }
