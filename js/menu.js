1b1e558659a6b8d4c17b61cb645f5a69c4d900a1
Michael Fienen <fienen@gmail.com>
2021-09-21 18:24:31 -0500

    WEB-9579: Fix open/close behavior on self-click

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@
  export default class Menu {
   constructor() {
     let navbar = document.querySelector('.navbar');
-    let menus = navbar.querySelectorAll('details');
+    let menus  = navbar.querySelectorAll('details');
 
     // Close an open menu if another menu item is opened
     menus.forEach((thisDetail, _, details) => {
@@ -16,11 +16,12 @@
     });
 
     // Close open menus if the user clicks on a non-menu item
-    document.addEventListener('click', function() {
-      menus.forEach((thisDetail) => {
-        if(thisDetail.open) thisDetail.removeAttribute('open');
-      });
-      return;
+    document.addEventListener('pointerdown', function(e) {
+      if(navbar !== e.target && !navbar.contains(e.target)) {
+        menus.forEach((thisDetail) => {
+          if(thisDetail.open) thisDetail.removeAttribute('open');
+        });
+      }
     });
 
     // Close open menus if the esc key is pressed
